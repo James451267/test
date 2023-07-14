f8fcdea9586e68983fd25674e294fc13e13f5718
James <139570219+James451267@users.noreply.github.com>
2023-07-14 11:26:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,80 @@
 document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
+
 var button = document.getElementById("clickme"),
   count = 0;
 button.onclick = function() {
   count += 1;
   button.innerHTML = "Button Clicks: " + count;
 };
+
+function dm() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
+
+function changeBackgroundColor() {
+   document.getElementById("box").style.backgroundColor = getRandomColor();   
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function getname() {
+  let person = prompt("Please enter your name", "John Doe");
+  if (person != null) {
+    document.getElementById("name").innerHTML =
+    "Hello " + person + ".";
+  }
+}
+
+var x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
+
+function showError(error) {
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      x.innerHTML = "User denied the request for Geolocation."
+      break;
+    case error.POSITION_UNAVAILABLE:
+      x.innerHTML = "Location information is unavailable."
+      break;
+    case error.TIMEOUT:
+      x.innerHTML = "The request to get user location timed out."
+      break;
+    case error.UNKNOWN_ERROR:
+      x.innerHTML = "An unknown error occurred."
+      break;
+  }
+}
+
+function clickCounter() {
+  if (typeof(Storage) !== "undefined") {
+    if (localStorage.clickcount) {
+      localStorage.clickcount = Number(localStorage.clickcount)+1;
+    } else {
+      localStorage.clickcount = 1;
+    }
+    document.getElementById("result").innerHTML = "You have clicked this button " + localStorage.clickcount + " time(s).";
+  } else {
+    document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+  }
+}
+
